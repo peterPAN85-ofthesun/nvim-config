4cfddd60845fdd008fae7cc3a4be91940b4ce0e1
peterPAN85-ofthesun <pineaugregoire85@gmail.com>
2025-09-24 18:06:28 +0200

    Vim-Plug

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
 require("core")
 require("config.lazy")
 
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug 'tpope/vim-sensible'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'jakemason/ouroboros'
+
+vim.call('plug#end')
