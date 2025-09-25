c38cc7cc8c648c17ce0206c267d8272061da118b
peterPAN85-ofthesun <pineaugregoire85@gmail.com>
2025-09-25 22:29:04 +0200

    UpdateLSP

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -42,6 +42,67 @@ return {
 		{ "<leader>F",  "<cmd>lua vim.lsp.buf.format({async = true})<cr>", desc = "Format buffer",                               mode = { "n", "x" } },
 		{ "<leader>rs", ":LspRestart<CR>",                                 desc = "Restart LSP",                                 mode = "n" },
 	},
+
+
+
+
+
+
+
+
+	opts = {
+		servers = {
+			-- Ensure mason installs the server
+			clangd = {
+				keys = {
+					{ "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+				},
+				root_markers = {
+					"compile_commands.json",
+					"compile_flags.txt",
+					"configure.ac", -- AutoTools
+					"Makefile",
+					"configure.ac",
+					"configure.in",
+					"config.h.in",
+					"meson.build",
+					"meson_options.txt",
+					"build.ninja",
+					".git",
+				},
+				capabilities = {
+					offsetEncoding = { "utf-16" },
+				},
+				cmd = {
+					"clangd",
+					"--background-index",
+					"--clang-tidy",
+					"--header-insertion=iwyu",
+					"--completion-style=detailed",
+					"--function-arg-placeholders",
+					"--fallback-style=llvm",
+				},
+				init_options = {
+					usePlaceholders = true,
+					completeUnimported = true,
+					clangdFileStatus = true,
+				},
+			},
+		},
+		setup = {
+			clangd = function(_, opts)
+				local clangd_ext_opts = LazyVim.opts("clangd_extensions.nvim")
+				require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+				return false
+			end,
+		},
+	},
+
+
+
+
+
+
 	config = function()
 		-- Customize error signs
 		vim.diagnostic.config({
