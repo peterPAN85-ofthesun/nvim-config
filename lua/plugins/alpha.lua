e25f1effc50b3acbb63908fb3b02d345e519d06a
Peter Pan <pineaugregoire85@gmail.com>
2026-01-17 23:18:03 +0100

    NewFile in nvim-alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -23,6 +23,7 @@ return {
     -- Menu buttons
     dashboard.section.buttons.val = {
       dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+      dashboard.button("n", "  New file", ":enew<CR>"),
       dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
       dashboard.button("g", "  Find text", ":Telescope live_grep<CR>"),
       dashboard.button("c", "  Configuration", ":e $MYVIMRC<CR>"),
